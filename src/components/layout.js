2bda3ff8cac705ce25680a5ddaca0a746b6bca1a
mbernardes19 <bernardes.matheus@outlook.com>
2020-06-14 17:57:43 -0300

    :art: Added main image to first page

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,15 +31,13 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: '960px',
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+
         </footer>
       </div>
     </div>
