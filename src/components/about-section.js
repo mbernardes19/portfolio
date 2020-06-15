9b2e0db0909ba053aa57af06dc5d4c474c04d023
mbernardes19 <bernardes.matheus@outlook.com>
2020-06-15 16:51:14 -0300

    :recycle: Refactors Section component to make resizable and placeable

--- src/components/about-section.js
+++ src/components/about-section.js
@@ -4,6 +4,14 @@ import ContentEN from '../../public/content/en/about/content.json'
 
 export default function AboutSection() {
     return(
-        <Section title={ContentEN.title} content={ContentEN.content} link="about"></Section>
+        <Section
+            title={ContentEN.title}
+            content={ContentEN.content}
+            link='about'
+            width='620px'
+            center
+            topMargin='2rem'
+            bottomMargin='2rem'
+        ></Section>
     );
 }
--- src/components/section.js
+++ src/components/section.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Section({title, content, link, sectionHeader, sectionBody}) {
+export default function Section({title, content, link, sectionHeader, sectionBody, width, center, topMargin, bottomMargin}) {
     const DefaultSectionHeader = () => (
         <SectionHeader>
             <SectionTitle>{title}</SectionTitle>
@@ -15,7 +15,7 @@ export default function Section({title, content, link, sectionHeader, sectionBod
     );
 
     return (
-        <SectionContainer id={link}>
+        <SectionContainer id={link} width={width} center={center} topMargin={topMargin} bottomMargin={bottomMargin}>
             { sectionHeader ? sectionHeader : <DefaultSectionHeader /> }
             { sectionBody ? sectionBody : <DefaultSectionBody /> }
         </SectionContainer>
@@ -23,16 +23,25 @@ export default function Section({title, content, link, sectionHeader, sectionBod
 }
 
 export const SectionContainer = styled.section`
-    width: 100%;
+    max-width: ${props => props.width ? props.width : '100%'};
+    margin: ${props => props.center ? '0 auto' : '' };
+    margin-top: ${props => props.topMargin ? props.topMargin : ''};
+    margin-bottom: ${props => props.bottomMargin ? props.bottomMargin : ''};
 `;
 
 export const SectionHeader = styled.header`
-
+    display: flex;
+    flex-flow: column wrap;
+    font-size: ${props => props.fontSize};
+    align-items: ${props => props.center ? 'center' : props.left ? 'flex-start' : props.right ? 'flex-end' : ''}
 `;
 
 export const SectionTitle = styled.h2`
 `;
 
 export const SectionBody = styled.article`
-
+    display: flex;
+    flex-flow: column wrap;
+    font-size: ${props => props.fontSize};
+    align-items: ${props => props.center ? 'center' : props.left ? 'flex-start' : props.right ? 'flex-end' : ''}
 `;
