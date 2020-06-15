ae2f61ce3587edb78ecfad27639cc7206897d578
mbernardes19 <bernardes.matheus@outlook.com>
2020-06-15 16:55:00 -0300

    :construction: Adds content from JSON to Header component

--- src/components/portfolio-section.js
+++ src/components/portfolio-section.js
@@ -6,7 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 // import SwipeableViews from 'react-swipeable-views';
-import TabPanel from './tabs';
+// import TabPanel from './tabs';
 
 
 
