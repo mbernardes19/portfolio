18d68d776e45d377e663a26a1cbae037c7e52cdb
mbernardes19 <bernardes.matheus@outlook.com>
2020-06-18 02:05:12 -0300

    :globe_with_meridians: Creates language configuration and context

---
+++ src/content/language-context.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import langs from './language';
+export default React.createContext(langs.ptBr);
---
+++ src/content/language.js
@@ -0,0 +1,155 @@
+
+const langs = {
+    ptBr: {
+        home: {
+            title: "Home"
+        },
+        about: {
+            title: "Sobre",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac velit odio. Pellentesque risus tellus, facilisis lobortis orci ut, ultricies finibus enim. Pellentesque volutpat pretium semper. Etiam vel nibh vitae augue ultricies vehicula eu at turpis. Vivamus elementum pharetra justo, ac scelerisque metus tincidunt ut. Praesent nec laoreet odio. Etiam suscipit et ligula sit amet ullamcorper. Cras viverra tellus risus, vel fermentum elit eleifend ut. Ut purus dolor, pulvinar eu felis nec, facilisis mollis leo. Praesent ipsum ante, rhoncus ac nunc et, volutpat posuere justo. Suspendisse aliquam, diam vitae iaculis finibus, purus sapien iaculis tortor, nec dapibus leo augue et sapien. Duis a purus pulvinar, sollicitudin libero a, congue massa. Vestibulum porta enim at lectus luctus, in varius dui efficitur. Donec egestas magna metus, vel porta leo placerat id."
+        },
+        blog: {},
+        hero: {
+            "greeting": "Oi, meu nome é",
+            "name": "Matheus",
+            "content": "Sou Desenvolvedor Frontend, dou aulas de programação e às vezes tento fazer uns jogos."
+        },
+        navLinks: {
+            "home": "Home",
+            "portfolio": "Portfólio",
+            "blog": "Blog",
+            "about": "Sobre"
+        },
+        portfolio: {
+            "title": "Portfólio",
+            "projects": [
+                {
+                    "title": "Beer Bank",
+                    "description": "UI developed to display beer data from PunkAPI",
+                    "itemsTitle": "Development tools",
+                    "items": ["React", "Material UI", "Fetch API", "Local Storage API"],
+                    "links": [
+                        {
+                            "title": "Deployed website",
+                            "url": "https://mbernardes19.github.io/beer-bank/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/beer-bank"
+                        }
+                    ]
+                },
+                {
+                    "title": "NG-Pokédex",
+                    "description": "",
+                    "itemsTitle": "",
+                    "items": [""],
+                    "links": [
+                        {
+                            "title": "Deployed web site",
+                            "url": "https://stackblitz.com/github/mbernardes19/ng-pokedex/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/ng-pokedex"
+                        }
+                    ]
+                },
+                {
+                    "title": "CobrinhaJS",
+                    "description": "",
+                    "itemsTitle": "",
+                    "items": [""],
+                    "links": [
+                        {
+                            "title": "Deployed web site",
+                            "url": "https://mbernardes19.github.io/cobrinha-js/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/cobrinha-js"
+                        }
+                    ]
+                }
+            ]
+        }
+    
+    },
+    en: {
+        home: {
+            title: "Home"
+        },
+        about: {
+            title: "About",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac velit odio. Pellentesque risus tellus, facilisis lobortis orci ut, ultricies finibus enim. Pellentesque volutpat pretium semper. Etiam vel nibh vitae augue ultricies vehicula eu at turpis. Vivamus elementum pharetra justo, ac scelerisque metus tincidunt ut. Praesent nec laoreet odio. Etiam suscipit et ligula sit amet ullamcorper. Cras viverra tellus risus, vel fermentum elit eleifend ut. Ut purus dolor, pulvinar eu felis nec, facilisis mollis leo. Praesent ipsum ante, rhoncus ac nunc et, volutpat posuere justo. Suspendisse aliquam, diam vitae iaculis finibus, purus sapien iaculis tortor, nec dapibus leo augue et sapien. Duis a purus pulvinar, sollicitudin libero a, congue massa. Vestibulum porta enim at lectus luctus, in varius dui efficitur. Donec egestas magna metus, vel porta leo placerat id."
+        },
+        blog: {},
+        hero: {
+            greeting: "Hi, I'm",
+            name: "Matheus",
+            content: "I'm a frontend developer, I also teach programming and sometimes I try to make games"
+        },
+        navLinks: {
+            home: "Home",
+            portfolio: "Portfólio",
+            blog: "Blog",
+            about: "About"
+        },
+        portfolio: {
+            "title": "Portfolio",
+            "projects": [
+                {
+                    "title": "Beer Bank",
+                    "description": "UI developed to display beer data from PunkAPI",
+                    "itemsTitle": "Development tools",
+                    "items": ["React", "Material UI", "Fetch API", "Local Storage API"],
+                    "links": [
+                        {
+                            "title": "Deployed website",
+                            "url": "https://mbernardes19.github.io/beer-bank/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/beer-bank"
+                        }
+                    ]
+                },
+                {
+                    "title": "NG-Pokédex",
+                    "description": "",
+                    "itemsTitle": "",
+                    "items": [""],
+                    "links": [
+                        {
+                            "title": "Deployed web site",
+                            "url": "https://stackblitz.com/github/mbernardes19/ng-pokedex/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/ng-pokedex"
+                        }
+                    ]
+                },
+                {
+                    "title": "CobrinhaJS",
+                    "description": "",
+                    "itemsTitle": "",
+                    "items": [""],
+                    "links": [
+                        {
+                            "title": "Deployed web site",
+                            "url": "https://mbernardes19.github.io/cobrinha-js/"
+                        },
+                        {
+                            "title": "Source code",
+                            "url": "https://github.com/mbernardes19/cobrinha-js"
+                        }
+                    ]
+                }
+            ]
+        }
+    
+    }
+}
+
+export default langs;
