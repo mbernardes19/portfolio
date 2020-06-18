6d55a60e573fda44cadb777b90e177b1040e6931
mbernardes19 <matheus.bernardes@thoughtworks.com>
2020-06-18 00:52:00 -0300

    :art: Moves Hero image to Hero section

--- src/components/section/hero-section.js
+++ src/components/section/hero-section.js
@@ -4,18 +4,31 @@ import ContentEN from '../../content/en/hero/content.json';
 import { SectionHeader, SectionBody, SectionTitle } from './section';
 import styled from 'styled-components';
 import placement from './placement';
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 export default function HeroSection() {
+    const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "matheus.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
     const HeroSectionHeader = () => (
         <SectionHeader center fontSize='2rem'>
-            <p style={{margin: 0}}>{ContentEN.greeting}</p>
-            <h1 style={{fontFamily: 'Roboto'}}>{ContentEN.name}</h1>
+            <p style={{margin: 0, fontWeight: 400, fontSize: '1.5rem', marginBottom: '.3rem'}}>{ContentEN.greeting}</p>
+            <h1 style={{fontFamily: 'Roboto', fontWeight: 500, fontSize: '3rem'}}>{ContentEN.name}</h1>
         </SectionHeader>
     )
     
     const HeroSectionBody = () => (
         <SectionBody center fontSize='1.2rem'>
-            <p style={{textAlign: 'center'}}>{ContentEN.content}</p>
+            <p style={{textAlign: 'center', fontWeight: 400, marginBottom: '2.5rem'}}>{ContentEN.content}</p>
         </SectionBody>
     )
 
@@ -26,20 +39,22 @@ export default function HeroSection() {
         aboutSectionPlacement.left = false;
         aboutSectionPlacement.right = false;
         aboutSectionPlacement.topMargin = '1rem';
-        aboutSectionPlacement.bottomMargin = '2rem';
-        console.log(aboutSectionPlacement);
+        aboutSectionPlacement.bottomMargin = '10rem';
         return aboutSectionPlacement;
     };
 
     return(
-        <Section
-            title={ContentEN.title}
-            content={ContentEN.content}
-            link="/"
-            sectionHeader={<HeroSectionHeader />}
-            sectionBody={<HeroSectionBody />}
-            placement={setPlacement()}
-        />
+        <div style={{ margin: '0 auto', padding: '20px 40px 0px 40px', maxWidth: `620px` }}>
+        <Img fluid={data.file.childImageSharp.fluid} />
+            <Section
+                title={ContentEN.title}
+                content={ContentEN.content}
+                link="/"
+                sectionHeader={<HeroSectionHeader />}
+                sectionBody={<HeroSectionBody />}
+                placement={setPlacement()}
+            />
+        </div>
     );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import PageBase from '../components/page-base';
 import AboutSection from '../components/section/about-section';
@@ -10,9 +9,6 @@ import FeatureToggle from '../components/feature-toggle';
 const HomePage = () => (
     <PageBase>
         <SEO title="Home" />
-        <div style={{ margin: '0 auto', padding: '20px 40px 0px 40px', maxWidth: `620px` }}>
-            <Image />
-        </div>
         <HeroSection />
         <FeatureToggle disabled>
             <AboutSection/>
