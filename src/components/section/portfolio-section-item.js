0d0ad62f039c675e95aa36cc047a3373071fdddb
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-19 18:04:12 -0300

    :lipstick: Adds Gif player to bot project gif

--- src/components/section/portfolio-section-item.js
+++ src/components/section/portfolio-section-item.js
@@ -4,10 +4,12 @@ import Button from '@material-ui/core/Button';
 import CodeRounded from '@material-ui/icons/CodeRounded';
 import LanguageRounded from '@material-ui/icons/LanguageRounded';
 import Gif from '../../images/dev-telegram-bot.gif';
+import GifPlayer from 'react-gif-player'
+import './gifplayer.css'
 
 export default function PortfolioSectionItem({type, content, image}) {
     const ProjectImage = () => {
-        return content.hasGif ? <img src={Gif} /> : image;
+        return content.hasGif ? <GifPlayer gif={Gif} style={{maxWidth: '100%', minWidth: '30%', width: '300px'}} /> : image;
     }
 
     const PrimaryButton = ({link, children}) => {
